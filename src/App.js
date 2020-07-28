1d74d20b5a6afeea1bc35c2265e7d4b26bea7732
Deborah Mendes <deborahmendes@pop-os.localdomain>
2020-07-28 10:04:37 +0530

    Guessing a single number done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import React,{Component} from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Form from './components/Form/Form';
+import Progress from './components/ProgressBar/ProgressBar';
 import './App.css';
 
 export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      Random:(Math.floor(Math.random()*100)),
-      occurence:[0]
+      Random:0,
+      color:"fff",
+      percentage:0
     }
+  }
+  componentDidMount(){
+    this.setState({Random:(Math.floor(Math.random()*100)),color:"#fff", percentage:0})
   }
   updateState(guess){
     const diff = Math.abs(guess - this.state.Random)
     console.log(diff)
     if(diff === 0 ){
-      alert("Correct")
-      this.setState({occurence:this.state.occurence.push})
+      this.setState({color:"#00FF00",percentage:100})
+    }
+    else if(diff>0 && diff<=4){
+      this.setState({color:"#FF0000", percentage:75})
+    }
+    else if(diff>4 && diff<=15){
+      this.setState({color:"#FFFF00", percentage:50})
+    }
+    else{
+      this.setState({color:"#00FFFF", percentage:25})
     }
   }
 
   render(){
-    let count = this.state.occurence
     return(
       <div className="App">
       <Navbar/>
       <div className="container">
-      { count.map((c,k) => (
-        <Form key={k} GuessTop={guess =>this.updateState(guess)}/>
-      ))
-      }
+        <div className="container1">
+          <Form GuessTop={guess =>this.updateState(guess)}/>
+          <Progress color={this.state.color} percentage={this.state.percentage}/>
+        </div>
+        <p>Hot</p>
       </div>
       <div className="btnc">
-        <button className="quit">QUIT</button>
         <p className="copy">&copy;DeborahMendes2020</p>
       </div>
     </div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './Form.module.css';
-import SemiCircleProgressBar from "react-progressbar-semicircle";
 
 const Form = ({GuessTop}) => {
     const onSubmit = (e) => {
@@ -9,15 +8,12 @@ const Form = ({GuessTop}) => {
         GuessTop(guess);
     }
     return(
-        <div onSubmit={onSubmit} className={styles.container}>
+        <div onSubmit={onSubmit}>
             <form>
                 <p className={styles.label}>Guess a number(1-100)</p>
                 <input className={styles.input} type="number" name="guess" placeholder="Enter Number"/>
                 <input type="submit" className={styles.btn} value="GUESS"/>
             </form>
-            <div className={styles.progress}>
-                <SemiCircleProgressBar percentage={25} stroke={"#0062FF"} background={"#fff"} strokeWidth={20} diameter={350}/>
-            </div>
         </div>
     ) 
 }
